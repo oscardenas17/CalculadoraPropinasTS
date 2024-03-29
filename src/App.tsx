41d9afcbe17af1f04cfcd652ca2bbaaa6e95bc05
oscardenas <oscardenas17@gmail.com>
2024-03-29 12:08:48 -0500

    Contenedores principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { menuItems } from "./data/db";
 
 function App() {
-
-
   return (
     <>
-  
+      <header
+        className="bg-teal-400 py-5
+  "
+      >
+        <h1 className="text-center text-4xl font-black">
+          {" "}
+          Cálculadora de propinas y consumo
+        </h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div>
+         
+          <h2>menu</h2>
+        </div>
+
+        <div>
+        
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
